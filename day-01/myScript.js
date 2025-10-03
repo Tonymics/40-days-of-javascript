2341da2adebc5182e583146e6ba8fdc02c4d6af7
Agbaire Onoriode Anthony <anthonyagbaire@gmail.com>
2025-10-03 10:16:04 +0100

    Assignment One done

---
+++ day-01/myScript.js
@@ -0,0 +1,7 @@
+console.log("Hello, tapaScript!");
+
+console.log("Welcome to 40 Days of JavaScript!");
+
+document.getElementById("myId").innerText = "Check the browser console for a message!";
+
+//document.write("Check the browser console for a message!");
