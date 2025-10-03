5a245d797cd7f6f6d0d44c1eb16632eaebf729fc
Agbaire Onoriode Anthony <anthonyagbaire@gmail.com>
2025-10-03 12:50:14 +0100

    Assignment/Task 2

---
+++ day-02/myScript.js
@@ -0,0 +1,25 @@
+let personName = "John Smith";
+let age = 22;
+let isStudentStatus = true;
+let favoriteProgLang = "JavaScript";
+
+console.log(personName);
+console.log(age);
+console.log(isStudentStatus);
+console.log(favoriteProgLang);
+
+let person = {
+    name: "John Doe",
+    age: 23,
+    isOnline: true
+}
+
+person.age = 20;
+
+console.log(person);
+
+let goodFruits = ["Banana", "Pineapple", "Orange"];
+
+goodFruits.push("Apple");
+
+console.log(goodFruits);
